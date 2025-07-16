e285d6cf7001ffb5dcfce9239b75caff8595d7bd
delaquash <olaide1191@gmail.com>
2025-07-16 14:26:42 +0100

    fix adminController

---
+++ backend/controllers/adminController.ts
@@ -0,0 +1,9 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+
+export const addDoctor = async (req: Request, res: Response, next: NextFunction) => {}
+export const allDoctors = async (req: Request, res: Response, next: NextFunction) => {}
+export const loginAdmin = async (req: Request, res: Response, next: NextFunction) => {}
+export const appointmentsAdmin = async (req: Request, res: Response, next: NextFunction) => {}
+export const appointmentCancel = async (req: Request, res: Response, next: NextFunction) => {}
+export const adminDashboard = async (req: Request, res: Response, next: NextFunction) => {}
---
+++ backend/middlewares/authAmdin.ts
@@ -0,0 +1,3 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+export const authAdmin = (req: Request, res: Response, next: NextFunction) => {}
