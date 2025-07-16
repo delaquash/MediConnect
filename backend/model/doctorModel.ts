9fe734fec1cc9d701577a2ac6c92c28532bfb022
delaquash <olaide1191@gmail.com>
2025-07-16 15:20:56 +0100

    feat: add doctor model

---
+++ backend/model/doctorModel.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+export interface IDoctor extends Document {
+  name: string;
+  email: string;
+  password: string;
+  image: string;
+  speciality: string;
+  degree: string;
+  experience: string;
+  about: string;
+  available: boolean;
+  fees: number;
+  address: {
+    [key: string]: any;
+  };
+  date: number;
+  slots_booked: {
+    [key: string]: any;
+  };
+}
+
+const doctorSchema = new Schema<IDoctor>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, default: true },
+    fees: { type: Number, required: true },
+    address: { type: Object, required: true },
+    date: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
+
+const DoctorModel: Model<IDoctor> =
+  mongoose.models.doctor || mongoose.model<IDoctor>("doctor", doctorSchema);
+
+export default DoctorModel;
