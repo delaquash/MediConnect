1f2176dd581a3dd6ed458f7cfd0df653af096832
delaquash <olaide1191@gmail.com>
2025-07-16 12:03:40 +0100

    feat: add images

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,27 @@
-import {Request, Response } from 'express';
+import "dotenv/config";
+import express, {Request, Response } from 'express';
+import { cors } from "cors";
+import connectDB from "./config/database";
+import connectCloudinary from "./config/cloudinary";
+
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+// app.use("/api/admin", adminRouter);
+// app.use("/api/doctor", doctorRouter);
+// app.use("/api/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => console.log("Server started", port));
