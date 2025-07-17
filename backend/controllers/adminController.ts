e1cec733d42cf2c67523ea01b1bfcdb42e773f0c
delaquash <olaide1191@gmail.com>
2025-07-17 17:43:49 +0100

    feat: add login controller

--- backend/controllers/adminController.ts
+++ backend/controllers/adminController.ts
@@ -1,7 +1,56 @@
 import express, { NextFunction, Request, Response } from 'express';
+import DoctorModel from '../model/doctorModel';
+import validator from 'validator';
+
+export const addDoctor = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name, email, password, specialty, degree, experience, about, available, fees, address } = req.body;
+        if(!name || !email || !password || !specialty || !degree || !experience || !about || !fees || !address) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        if (!validator.isStrongPassword(password, {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+        })) {return res.status(400).json({
+            message:
+            "Password must be at least 8 characters long and include uppercase, lowercase, numbers, and symbols",
+        });
+        }
 
 
-export const addDoctor = async (req: Request, res: Response, next: NextFunction) => {}
+        const doctor = new DoctorModel({
+            name,
+            email,
+            password,
+            specialty,
+            degree,
+            experience,
+            about,
+            available,
+            fees,
+            address,
+            date: Date.now(),
+        });
+
+        const SavedDoctor = await doctor.save();
+        res.status(201).json({ 
+            status: "success",
+            message: "Doctor added successfully", 
+            data: SavedDoctor,
+         });
+    } catch (error) {
+        next(error);
+        
+    }
+}
 export const allDoctors = async (req: Request, res: Response, next: NextFunction) => {}
 export const loginAdmin = async (req: Request, res: Response, next: NextFunction) => {}
 export const appointmentsAdmin = async (req: Request, res: Response, next: NextFunction) => {}
--- backend/model/doctorModel.ts
+++ backend/model/doctorModel.ts
@@ -1,3 +1,4 @@
+import { timeStamp } from "console";
 import mongoose, { Document, Schema, Model } from "mongoose";
 
 export interface IDoctor extends Document {
@@ -5,7 +6,7 @@ export interface IDoctor extends Document {
   email: string;
   password: string;
   image: string;
-  speciality: string;
+  specialty: string;
   degree: string;
   experience: string;
   about: string;
@@ -26,7 +27,7 @@ const doctorSchema = new Schema<IDoctor>(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     image: { type: String, required: true },
-    speciality: { type: String, required: true },
+    specialty: { type: String, required: true },
     degree: { type: String, required: true },
     experience: { type: String, required: true },
     about: { type: String, required: true },
@@ -36,7 +37,11 @@ const doctorSchema = new Schema<IDoctor>(
     date: { type: Number, required: true },
     slots_booked: { type: Object, default: {} },
   },
-  { minimize: false }
+  { minimize: false, // Prevents mongoose from removing empty objects
+    timestamps: true   // Automatically adds createdAt and updatedAt fields
+  }, 
+  
+
 );
 
 const DoctorModel: Model<IDoctor> =
