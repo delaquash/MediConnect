f2910c640d97339b5f1fcf3cc6f5400e3a05d7e2
delaquash <olaide1191@gmail.com>
2025-08-28 10:27:59 +0100

    feat: add related doctors page

--- frontend/src/routes/appointments.tsx
+++ frontend/src/routes/appointments.tsx
@@ -1,11 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useParams } from '@tanstack/react-router'
 import { assets } from '../assets/assets'
+import { useState } from 'react'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 export const Route = createFileRoute('/appointments')({
   component: Appointment,
 })
 
 function Appointment() {
+  // const { docId } = useParams()
+  // const { doctors, currencySymbol, backendUrl, token, getDoctosData } = useContext(AppContext)
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+
+  const [docInfo, setDocInfo] = useState(false)
+  const [docSlots, setDocSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
+
   return (
     <>
       <div>
@@ -40,13 +51,33 @@ function Appointment() {
             <p className='text-gray-600 font-medium mt-4'>Appointment fee: <span className='text-gray-800'>#70,000</span> </p>
           </div>
         </div>
-      </div>
+     
       {/* Booking slot */}
       <div className='sm:ml-72 sm:pl-4 mt-8 font-medium text-[#565656] text-2xl'>
         <p>Booking Slot</p>
-        <div className="">
-          
+        <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+          {docSlots.length && docSlots.map((doc, index) => (
+            <div
+              key={index}
+              className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? "bg-[#5F6FFF] text-white" : "border border-[#DDDDDD]"}`}>
+              {/* <p>{doc[0] && daysOfWeek[doc[0].datetime?.getDay()]}</p>
+                <p>{doc[0] && doc[0].datetime?.getDate()}</p> */}
+            </div>
+          ))}
         </div>
+
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          {docSlots.length && docSlots[slotIndex].map((item, index) => (
+            <p onClick={() => setSlotTime(item.time)} key={index} className={`text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-[#949494] border border-[#B4B4B4]'}`}>{item.time.toLowerCase()}</p>
+          ))}
+          </div>
+
+          <button className='bg-primary text-white text-sm font-light px-20 py-3 rounded-full my-6'>Book an appointment</button>
+        </div>
+      </div> 
+      {/* </div> */}
+      <div>
+        <RelatedDoctors />
       </div>
     </>
   )
